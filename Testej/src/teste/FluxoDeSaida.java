7b63d965d106aa6d488718ef35e6b926be26e0ba
Paulo Leão <ptalregister@gmail.com>
2025-08-07 17:43:58 -0300

    Output Flow
    Understanding the output flow.

---
+++ Testej/src/teste/FluxoDeSaida.java
@@ -0,0 +1,14 @@
+package teste;
+
+public class FluxoDeSaida {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Impressão no Java");
+		System.out.print("Operação: ");
+		System.out.println(2+3);
+		System.out.println("Fim " + "Teste");
+		
+	}
+
+}
