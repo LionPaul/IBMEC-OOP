eef543ab916b394f7b3b8b6da4028744237492e2
Paulo Leão <ptalregister@gmail.com>
2025-08-07 17:46:03 -0300

    Variables Declaration
    Understanding the variables declaration.

---
+++ Testej/src/teste/DeclaracaoDeVariavel.java
@@ -0,0 +1,24 @@
+package teste;
+
+public class DeclaracaoDeVariavel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String nome;
+		int idade;
+		double peso;
+		char sexo;
+		
+		nome = "Carlos Eduardo";
+		idade = 32;
+		peso = 80.5;
+		sexo = 'M';
+		
+		System.out.println(nome);
+		System.out.println(idade);
+		System.out.println(peso);
+		System.out.println(sexo);
+	}
+
+}
